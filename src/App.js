5c35d7274a4e291efd2331af7acb02b6f820ab11
Gaurav6982 <gaurav.jss.027@gmail.com>
2020-11-01 17:29:43 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import logo from './logo.svg';
+
 import {Navbar,NavbarBrand} from 'reactstrap';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand> Coursera </NavbarBrand>
-          </div>
-      </Navbar>
-    </div>
-  );
+import React,{Component} from 'react'
+import Menu from './components/MenuComponent'
+import {DISHES} from './shared/dishes'
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render(){
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand> Coursera </NavbarBrand>
+            </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
 }
-
-export default App;
+  export default App;
--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,26 +1,23 @@
 import React,{Component} from 'react'
 import {Card,CardImg,CardTitle,CardBody,CardText} from 'reactstrap'
 class DishDetail extends Component{
-    renderDish(dish)
-    {
+    renderDish(dish) {
         if(dish!=null)
         {
-            return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
+        return(
+          <div className="col-12 col-md-5 m-1">
+              <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                   <CardTitle>{dish.name}</CardTitle>
+                   <CardText>{dish.description}</CardText>
+                </CardBody>
+              </Card>
+           </div>
+             );
         }
         else
-        {
-            return(
-                <div></div>
-            );
-        }
+        return(<div></div>);
     }
     renderComments(comments) {
         if(comments != null) {
@@ -60,11 +57,11 @@ class DishDetail extends Component{
         }
     }
     render(){
-        if (this.props.dish != null) {
+        if (this.props.selectedDish != null) {
             return(
                 <div className="row">
-                      {this.renderDish(this.props.dish)}
-                      {this.renderComments(this.props.dish.comments)}
+                      {this.renderDish(this.props.selectedDish)}
+                      {this.renderComments(this.props.selectedDish.comments)}
                 </div>
             );
           }
